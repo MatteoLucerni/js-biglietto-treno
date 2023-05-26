1443f20d04da812d321219d1dbcf42446384612a
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-05-26 17:57:56 +0200

    completed velidation

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 - Targetto un elemento dal DOM
 - Chiedo all'utente numero chilometri e l'età
 - Salvo i dati inseriti
-- Valido i dati inseriti
+- Valido i dati inseriti:
+    - Possono essere inseriti solo numeri
+    - La distanza non puo essere minore di 0
+    - L'età non può essere minore di 1
 - Calcolo il prezzo del biglietto
 - Calcolo se c'è lo sconto in base all'età
 - Applico lo sconto al prezzo calcolato dai dati inseriti
--- js/script.js
+++ js/script.js
@@ -20,30 +20,38 @@ const age = parseInt(prompt('Inserisci la tua età per calcolare lo sconto', '17
 console.log('age: ' + age);
 
 
-// TODO validazione
+// Validazione elementi inseriti
 
-// Calcolo prezzo biglietto
+if((typeof distance, typeof age) === 'number' && distance >= 0 && age > 0){
 
-const price = (distance * 0.21);
-console.log('price: ' + price);
+    console.log('Validazione completata correttamente');
 
-// Calcolo lo sconto in base all età
+    // Calcolo prezzo biglietto
 
-let discount = 0;
+    const price = (distance * 0.21);
+    console.log('price: ' + price);
 
-if(age < 18){
-    discount = 20;
-    console.log('Sei minorenne, lo sconto è del',discount,'%.');
-} else if(age >= 65){
-    discount = 40;
-    console.log('Sei over 65, lo sconto è del',discount,'%.');
+    // Calcolo lo sconto in base all età
+
+    let discount = 0;
+
+    if(age < 18){
+        discount = 20;
+        console.log('Sei minorenne, lo sconto è del',discount,'%.');
+    } else if(age >= 65){
+        discount = 40;
+        console.log('Sei over 65, lo sconto è del',discount,'%.');
+    }
+
+    // Calcolo il prezzo finale scontato
+
+    discountedPrice = (price - ((discount / 100) * price)).toFixed(2);
+    console.log('Prezzo scontato: ' + discountedPrice);
+
+    // Inserisco il prezzo scontato in pagina
+
+    target.innerText = `Il tuo biglietto costa: ${discountedPrice}€`;
+} else{
+    console.log('Error: Please enter only valid numbers!');
 }
 
-// Calcolo il prezzo finale scontato
-
-discountedPrice = (price - ((discount / 100) * price)).toFixed(2);
-console.log('Prezzo scontato: ' + discountedPrice);
-
-// Inserisco il prezzo scontato in pagina
-
-target.innerText = `Il tuo biglietto costa: ${discountedPrice}€`;
